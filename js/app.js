6ee41cf7822000d65fe17b56acf92986324e9a89
aaron-emerson <phobosnz@gmail.com>
2021-02-11 12:05:08 +1300

    Add comments to all files

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
+// Declare variables for text, speed for write speed, i for integer
 let i = 0;
 let speed = 100;
 let txt = "Ideas from the deep.";
 
+
+// function to animate the typing, starts from initial character left to right
 function typeWriter() {
  if (i < txt.length) {
   document.getElementById("title").innerHTML += txt.charAt(i);
